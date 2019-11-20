a5e53ce8020e7af6f1fda7286d29fdf9bea683b2
Andrew Grigorkin <a.grigorkin@outlook.com>
2019-11-20 14:15:34 +0100

    initial commit

---
+++ src/Providers/PaginationServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AndrewGrigorkin\Pagination\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class PaginationServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
+
